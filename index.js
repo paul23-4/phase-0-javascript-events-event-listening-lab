c8bfd6b6e1f112bde02d64943a08f3fb266d209b
paul <paul.thiongo@student.moringaschool.com>
2024-01-12 21:42:11 +0300

    Done

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
 function addingEventListener() {
+    const input = document.getElementById('input');
+
+    // Add an event listener
+    input.addEventListener('click', function() {
+        console.log('Input clicked!');
+    });
+}
+
+// Export addingEventListener for testing purposes
+if (typeof module !== 'undefined' && module.exports) {
+    module.exports = {
+        addingEventListener
+    };
 }
